ec4a04c64360479f0b4ec7185f1030ae65537573
joanna tan <2605678@sit.singaporetech.edu.sg>
2026-09-11 18:36:19 +0800

    while loop

--- auditor.py
+++ auditor.py
@@ -1,31 +1,12 @@
 inventory = 0
 failed_entries = 0
 
+# Keep asking for stock quantities continuously
 while True:
+
+    # Ask the user to enter a stock quantity
     stock = input("Enter stock quantity or 'quit': ")
 
+    # Stop the loop if the user enters "quit"
     if stock == "quit":
         break
-
-    if stock.isdigit():
-        stock = int(stock)
-
-        if stock < 0:
-            print("Error: Negative numbers are not allowed.")
-            failed_entries += 1
-            continue
-
-        inventory += stock
-        print("Inventory:", inventory)
-
-        if inventory > 500:
-            print("ALERT: Inventory exceeds 500 units!")
-            break
-
-    else:
-        print("Error: Invalid input.")
-        failed_entries += 1
-        continue
-
-print("Total Units Processed:", inventory)
-print("Number of Failed/Rejected Entries:", failed_entries)
